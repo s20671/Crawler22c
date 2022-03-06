fe23e850a86b30c1124e80a81696807176112fab
Mateusz Krutel <s20671@pjwstk.edu.pl>
2022-03-06 12:42:44 +0100

    Crawler1

---
+++ Crawler/Crawler/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawler
+{
+    class Class1 : Interface1
+    {
+        private double _myProperty;
+
+        public double MyProperty {
+            get { return _myProperty * 1.23; }
+            set { _myProperty = value; }
+
+        }
+        public int Zmienna { get; set; }
+
+        public void Metoda()
+        {
+            try { }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Crawler/Crawler/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Crawler
+{
+    class Program
+    {
+        public async static Task Main(string[] args)
+        {
+            var websiteUrl = args[0];
+            Console.WriteLine(websiteUrl);
+
+            var https = new HttpClient();
+
+            var response = await https.GetAsync(websiteUrl);
+            Console.WriteLine(response);
+
+            var content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(content); 
+            
+            var a = $"Content {content}";
+            var b = @"\.-]";
+            var regex = new Regex("([a-zA-Z0-9+._-]+@[a-zA-Z0-9._-]+\\.[a-zA-Z0-9_-]+)");
+
+
+            var matchCollection = regex.Matches(content);
+
+           
+
+            foreach(var item in matchCollection)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
